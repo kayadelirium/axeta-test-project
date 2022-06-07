df367d27eb4f7e9913ae7ddd7586b2553f65b1ac
ilya danilov <idanilov@inobitec.com>
2022-06-07 12:49:14 +0400

    little fix

--- src/layout/Col/index.styled.tsx
+++ src/layout/Col/index.styled.tsx
@@ -4,6 +4,5 @@ import { IStyledProps } from "./types";
 export const ColStyled = styled.section`
     flex: ${(props: IStyledProps) => (props.flex ? props.flex : 1)};
     min-width: 200px;
-    padding-right: 24px;
-    padding-bottom: 24px;
+    padding: 12px;
 `;
--- src/layout/Wrapper/index.styled.tsx
+++ src/layout/Wrapper/index.styled.tsx
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const WrapperStyled = styled.div`
     margin: auto;
     max-width: 960px;
+    padding: 0px 10px;
 `;
